feebf879b442bf6833c0332cdd87556972aa156a
Ayan Naskar <113054786+AyanNaska@users.noreply.github.com>
2023-09-10 14:02:20 +0530

    Add files via upload

---
+++ Wifi_Car.ino
@@ -0,0 +1,115 @@
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+
+
+
+// Define the motor pins
+#define ENA 12
+#define IN1 14
+#define IN2 27
+#define IN3 25
+#define IN4 26
+#define ENB 13
+
+// Variables for the Blynk widget values
+int x = 50;
+int y = 50;
+int Speed;
+
+char auth[] = "_S5HGiQqHxa4ykdK5ujlM5ObtETxcQo6"; //Enter your Blynk auth token
+char ssid[] = "AyanFi"; //Enter your WIFI name
+char pass[] = "lxgiwyluzumynw"; //Enter your WIFI passowrd
+
+void setup() {
+  Serial.begin(9600);
+  //Set the motor pins as output pins
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  // Initialize the Blynk library
+  Blynk.begin(auth, ssid, pass);
+}
+
+// Get the joystick values
+BLYNK_WRITE(V0) {
+  x = param[0].asInt();
+}
+// Get the joystick values
+BLYNK_WRITE(V1) {
+  y = param[0].asInt();
+}
+//Get the slider values
+BLYNK_WRITE(V2) {
+  Speed = param.asInt();
+}
+
+// Check these values using the IF condition
+void smartcar() {
+  if (y > 70) {
+    carForward();
+    Serial.println("carForward");
+  } else if (y < 30) {
+    carBackward();
+    Serial.println("carBackward");
+  } else if (x < 30) {
+    carLeft();
+    Serial.println("carLeft");
+  } else if (x > 70) {
+    carRight();
+    Serial.println("carRight");
+  } else if (x < 70 && x > 30 && y < 70 && y > 30) {
+    carStop();
+    Serial.println("carstop");
+  }
+}
+
+void loop() {
+  Blynk.run(); // Run the Blynk function
+  smartcar();  // Call the main function
+}
+
+/**************Motor movement functions*****************/
+void carForward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carBackward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carLeft() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carRight() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carStop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
